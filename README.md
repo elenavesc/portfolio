a9e12621e7feec8f45c4420217eee270d1b8a35a
Elena Valero <elenavaleroesco@gmail.com>
2024-05-04 20:38:24 +0200

    Added portfolio

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# portfolio
+## Elena Valero Portfolio
+
+Welcome to the portfolio of Elena Valero! Here you will find a comprehensive overview of Elena's professional and educational journey, showcasing her skills, experiences, and achievements.
+
+**About Elena Valero:**
+
+Elena is a dedicated professional with a passion for development. With a strong educational background and years of practical experience, she has established herself as a competent and driven individual in her field.
+
+**Professional Experience:**
+
+Elena has accumulated valuable experience through her roles in various organizations. Her career highlights include:
+
+- Web Developer.
+- Teacher.
+- Designer.
+
+**Educational Background:**
+
+Elena's educational journey has equipped her with the knowledge and skills necessary to excel in her field. Her academic achievements include:
+
+- Master 
+
+**Portfolio Highlights:**
+
+Within this portfolio, you'll find a selection of Elena's notable works, projects, and accomplishments. These may include:
+
+- Compartamos.
+
+**Why Choose Elena Valero:**
+
+Passionate about programming, always willing to learn something new. There is a new technology, tool, or framework to learn every day! Autodidact, with a knack for teamwork, able to be part of or even manage a small team. Capable of making decisions whenever necessary. And, of course, with a resilient and positive attitude!
+
+**Get in Touch:**
+
+If you're interested in collaborating with Elena or learning more about her work, feel free to reach out through the following channels:
+
+- Email: elenavaleroesco@gmail.com
+- LinkedIn: https://www.linkedin.com/in/elena-valero-371a711ba/
+
+Thank you for visiting Elena Valero's portfolio. We hope you find it informative and inspiring.
+
+**And in Spanish just in case**
+
+
+![CV Elena Valero Ecobar (3) (1)_page-0001](https://github.com/elenavesc/My-portfolio/assets/147326237/7e9a6eda-3a92-4d78-94cb-3f39936141d9)
+
+
+
+## 👀 Want to learn more?
+
+Feel free to check [Astro documentation](https://docs.astro.build) or jump into Astro [Discord server](https://astro.build/chat).
+
---
+++ src/data/info.ts
@@ -0,0 +1,94 @@
+export const info = {
+  name: "Elena Valero Escobar",
+  jobDescription: "Full stack developer",
+  about: `Passionate about programming, always willing to learn something new. There is a new technology, tool, or framework to learn every day! Autodidact, with a knack for teamwork, able to be part of or even manage a small team. Capable of making decisions whenever necessary. And, of course, with a resilient and positive attitude!
+  `,
+
+  experience: [
+    {
+      name: "Compartamos",
+      location: "Granada, Spain",
+      startDate: "Jan 2024",
+      endDate: "Present",
+      description: [
+        "Freelance Project: Development of an Application for Seniors Seeking a Life Companion.",
+      ],
+    },
+
+    {
+      name: "ESADA, Teacher",
+      location: "Granada, Spain",
+      startDate: "Oct 2023",
+      endDate: "Present",
+      description: [
+        "Specialized Teacher in CAD (Computer-Aided Design) across Various Levels of Design Education.",
+      ],
+    },
+
+    {
+      name: "Everythingflamenco, Prototyping Designer",
+      location: "Granada, Spain",
+      startDate: "Dec 2022",
+      endDate: "Jun 2023",
+      description: [
+        "Costume Design for International Dance Companies.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "ADALAB, Bootcamp full stack developer",
+      location: "Spain",
+      startDate: "2023",
+      endDate: "2024",
+      description: [
+        "Development of web applications, both frontend and backend.",
+      ],
+    },
+    {
+      name: "UGR, Master's in Pedagogical Training for Teaching",
+      location: "Granada, Spain",
+      startDate: "2022",
+      endDate: "2023",
+      description: ["Training for Teachers at Higher Levels of Education."],
+    },
+    {
+      name: "ESCO, Master's in Fashion Communication and Management",
+      location: "Granada, Spain",
+      startDate: "2021",
+      endDate: "2022",
+      description: ["Business Management of Fashion Brands."],
+    },
+    {
+      name: "EAG, Degree in Fashion Design",
+      location: "Granada, Spain",
+      startDate: "2017",
+      endDate: "2021",
+      description: ["Higher Artistic Studies in Fashion Design with a Specialization in Fashion Management."],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/elenavesc",
+    email: "mailto:elenavaleroesco@gmail.com",
+    linkedin: "https://www.linkedin.com/in/elena-valero-371a711ba/",
+  },
+
+  projects: [
+    {
+      title: "Compartamos",
+      isFeatured: true,
+      thumbnail: "/assets/images/3-removebg-preview.png",
+      githubUrl: "https://github.com/elenavesc/Compartamos",
+      liveUrl: "https://elenavesc.github.io/Compartamos/",
+    },
+    {
+      title: "Harry Potter API",
+      isFeatured: true,
+      thumbnail: "/assets/images/hp.jpg",
+      githubUrl: "https://github.com/elenavesc/Harry-Potter-List",
+      liveUrl: "https://beta.adalab.es/modulo-3-evaluacion-final-elenavesc/",
+    },
+  ],
+};
